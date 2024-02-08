68fe3ccb0df0e0c694d118bb675d895cea29ddcd
thearyan007 <thearyangupta007@gmail.com>
2024-02-08 14:04:26 +0530

    added LogOut function

--- FrontEnd/src/components/Sidebar/LogoutButton.jsx
+++ FrontEnd/src/components/Sidebar/LogoutButton.jsx
@@ -1,8 +1,10 @@
 import { BiLogOut } from "react-icons/bi";
+import useLogOut from "../../hooks/useLogOut";
 const LogoutButton =()=>{
+    const {loading, logOut} = useLogOut();
     return (
     <div className="mt-auto">
-        <BiLogOut className="w-7 h-7 cursor-pointer text-white"/>
+        {!loading?(<BiLogOut className="w-7 h-7 cursor-pointer text-white" onClick={logOut}/>):<span className="loading loading-spinner"></span>}
     </div>
 
     )
--- FrontEnd/src/pages/signup/SignUp.jsx
+++ FrontEnd/src/pages/signup/SignUp.jsx
@@ -105,7 +105,9 @@ const SignUp = () => {
           </Link>
 
         <div>
-        <button className='btn btn-block btn-sm mt-2'>Sign Up</button>
+        <button className='btn btn-block btn-sm mt-2' disabled={loading}>
+          {loading? <span className="loading loading-spinner text-white"></span>:"Sign Up"}
+          </button>
       </div>
 
       </form>
